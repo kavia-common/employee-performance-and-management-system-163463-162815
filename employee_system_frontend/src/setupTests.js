c909498bc910ccb5435cf69d68fc6476220b17ee
Kavia AI <production@kavia.ai>
2025-08-29 08:04:58 +0000

    CheckPoint - cg2886d8ed

--- employee_system_frontend/src/setupTests.js
+++ employee_system_frontend/src/setupTests.js
@@ -2,7 +2,6 @@
  // allows you to do things like:
  // expect(element).toHaveTextContent(/react/i)
  // learn more: https://github.com/testing-library/jest-dom
- import React from 'react';
 import '@testing-library/jest-dom';
 
  // Mock matchMedia for components or libs that check it
@@ -21,19 +20,20 @@ import '@testing-library/jest-dom';
    };
  }
 
- // Jest doesn't automatically use __mocks__ for ESM without mapper, but CRA/jest will prefer manual mocks in __mocks__ for commonjs.
- // Ensure recharts resolves to our mock in tests if test environment requires it.
+ // Ensure recharts is mocked without referencing React outside the factory
  try {
-  jest.mock('recharts', () => {
-    const Passthrough = (props) => {
-      const children = props && props.children;
-      const clone = {};
-      for (const k in props) { if (k !== "children" && Object.prototype.hasOwnProperty.call(props, k)) clone[k] = props[k]; }
-      return React.createElement('div', Object.assign({ 'data-testid': 'recharts-mock' }, clone), children);
-    };
-    return { __esModule: true, ResponsiveContainer: Passthrough, AreaChart: Passthrough, Area: Passthrough, Tooltip: Passthrough, XAxis: Passthrough, YAxis: Passthrough, BarChart: Passthrough, Bar: Passthrough, default: {} };
-  });
-} catch (e) {
-  // noop if jest is not available in this scope
-}
-
+   jest.mock('recharts', () => ({
+     __esModule: true,
+     ResponsiveContainer: () => null,
+     AreaChart: () => null,
+     Area: () => null,
+     Tooltip: () => null,
+     XAxis: () => null,
+     YAxis: () => null,
+     BarChart: () => null,
+     Bar: () => null,
+     default: {},
+   }));
+ } catch (e) {
+   // noop if jest is not available in this scope
+ }
